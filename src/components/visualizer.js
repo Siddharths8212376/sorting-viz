6cdb03963d732f47901e4fbebed34180fa5b1914
Siddharths8212376 <siddharth8212376@gmail.com>
2020-08-26 15:34:25 +0530

    initial commit

---
+++ src/components/visualizer.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import { render } from '@testing-library/react'
+
+const Visualizer = () => {
+   return (
+        <div>Visualizer Here!</div>
+    )
+}
+
+export default Visualizer
